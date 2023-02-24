d62189bfc45b3f7ebd01cbe8ec412b27ff9eab5f
Jozhué Beni <jbeeniif@gmail.com>
2023-02-24 19:16:43 -0300

    feat: tentando ajeitar o pagination com o sort

--- src/services/movies/listMovies.service.ts
+++ src/services/movies/listMovies.service.ts
@@ -1,6 +1,10 @@
 import { AppDataSource } from "../../data-source";
 import { Movie } from "../../entities";
-import { iMovieRepo, IPagination } from "../../interfaces/movies.interfaces";
+import {
+  iMovieRepo,
+  IPagination,
+  MovieAllReturn,
+} from "../../interfaces/movies.interfaces";
 import { returnAllMovies } from "../../schemas/movies.schemas";
 
 export const listMoviesService = async (payload: any): Promise<IPagination> => {
@@ -8,12 +12,17 @@ export const listMoviesService = async (payload: any): Promise<IPagination> => {
 
   const countMovies: number = await moviesRepository.count();
 
-  let page: number = Number(payload.page) > 0 ? payload.page : 1;
+  let page: number = Number(payload.page) > 0 ? Number(payload.page) : 1;
   const perPage: number =
-    Number(payload.perPage) === undefined ? 5 : payload.perPage;
+    Number(payload.perPage) > 0 && Number(payload.perPage) <= 5
+      ? Number(payload.perPage)
+      : 5 || 5;
+
+  const sort: any = payload.sort;
+  const order: any = payload.order;
 
   let prevPage: string | null =
-    page === 0
+    page === 1
       ? null
       : `http://localhost:3000/movies?page=${page - 1}&perPage=${perPage}`;
 
@@ -22,13 +31,44 @@ export const listMoviesService = async (payload: any): Promise<IPagination> => {
       ? null
       : `http://localhost:3000/movies?page=${page + 1}&perPage=${perPage}`;
 
-  const findMovies: Array<Movie> = await moviesRepository.find({
+  let findMovies: MovieAllReturn;
+
+  if (!sort) {
+    findMovies = await moviesRepository.find({
+      take: perPage,
+      skip: perPage * (page - 1),
+      order: {
+        id: "ASC",
+      },
+    });
+  }
+  if (sort && !order) {
+    if (sort == "duration") {
+      findMovies = await moviesRepository.find({
+        take: perPage,
+        skip: perPage * (page - 1),
+        order: {
+          duration: order,
+        },
+      });
+    } else if (sort == "price") {
+      findMovies = await moviesRepository.find({
+        take: perPage,
+        skip: perPage * (page - 1),
+        order: {
+          price: order,
+        },
+      });
+    }
+  }
+  findMovies = await moviesRepository.find({
     take: perPage,
     skip: perPage * (page - 1),
     order: {
-      durati: "ASC",
+      id: "ASC" || "DESC",
     },
   });
+
   const movies = returnAllMovies.parse(findMovies);
 
   const pagination: IPagination = {
